436d261425f86f9da9a5452c5fd6e28e1c60c4f9
tangkaifang <kaifang.tang@wenge.com>
2022-02-21 17:48:21 +0800

    init项目,且完成rollup配置,teacher是老师的版本可以用来对比

---
+++ packages/reactivity/dist/reactivity.global.js
@@ -0,0 +1,14 @@
+var VueReactivity = (function (exports) {
+    'use strict';
+
+    var index = {
+        a: 1,
+    };
+
+    exports["default"] = index;
+
+    Object.defineProperty(exports, '__esModule', { value: true });
+
+    return exports;
+
+})({});
